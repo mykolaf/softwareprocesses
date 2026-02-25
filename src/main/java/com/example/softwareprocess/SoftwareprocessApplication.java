2c4b051a6e38042afe5a0ce2328e1d4aa9c95f9e
Mykola Faryma <myk.faryma@gmail.com>
2026-02-25 08:34:56 +0100

    initial commit
    Signed-off-by: Mykola Faryma <myk.faryma@gmail.com>

---
+++ src/main/java/com/example/softwareprocess/SoftwareprocessApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwareprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareprocessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softwareprocess/SoftwareprocessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwareprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareprocessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
